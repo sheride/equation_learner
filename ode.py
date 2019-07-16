ce9e47bcb7db7f363c6d0d73ca0f8b1408aaccfe
Elijah Sheridan <43191104+sheride@users.noreply.github.com>
2019-07-16 14:57:17 -0400

    Misc, fixing local-remote problems

--- ode.py
+++ ode.py
@@ -13,13 +13,40 @@ import matplotlib.pyplot as plt
 
 
 def odeSolve(model, odeFunction, initialCond, timeSpan, step):
-    actualSol = solve_ivp(odeFunction, timeSpan, initialCond,
-                              first_step=step, max_step=step)
-    modelSol = solve_ivp(model.odecompat, timeSpan, initialCond,
-                              first_step=step, max_step=step)
+    actualSol = solve_ivp(odeFunction, timeSpan, initialCond, first_step=step,
+                          max_step=step)
+    modelSol = solve_ivp(model.odecompat, timeSpan, initialCond, 
+                         first_step=step, max_step=step)
     return [actualSol, modelSol]
 
-# FIG SIZE IS TUPLE W/ LENGTH 2
+
+def diffPlot(actualSol, modelSol, figsize=(10,10), name='Difference Plot'):
+    newTrue = 0
+    newModel = 0
+
+    if actualSol.y[0].shape[0] > modelSol.y[0].shape[0]:
+        newTrue = actualSol.y[0][0:modelSol.y[0].shape[0]]
+        newModel = modelSol.y[0]
+        t = modelSol.t
+    else:
+        newModel = modelSol.y[0][0:actualSol.y[0].shape[0]]
+        newTrue = actualSol.y[0]
+        t = actualSol.t
+
+    diff = np.abs(newTrue - newModel)
+
+    font = {'family': 'normal', 'weight': 'bold', 'size': 22}
+    plt.figure(figsize=figsize)
+    plt.suptitle('Title', **font)
+    plt.xlabel('X-Axis', **font)
+    plt.ylabel('Y-Axis', **font)
+    plt.xticks(fontsize=15)
+    plt.yticks(fontsize=15)
+    line, = plt.plot(t, diff)
+    plt.legend((line), ("Model 1"))
+    plt.savefig(name+'.png')
+
+
 def make2DMovie(actualSolCoords, modelSolCoords, xmin=-3, xmax=3, ymin=-3, 
                 ymax=3, figSize=(10,10), lineWidth=4, 
                 name='Model-Reality Comparison'):
@@ -40,20 +67,3 @@ def make2DMovie(actualSolCoords, modelSolCoords, xmin=-3, xmax=3, ymin=-3,
         plt.yticks(fontsize=15)
         movie.gather()
     movie.finalize()
-
-
-def diffPlot(actualSol, modelSol):
-    newTrue = 0
-    newModel = 0
-
-    if actualSol.y[0].shape[0] > modelSol.y[0].shape[0]:
-        newTrue = actualSol.y[0][0:modelSol.y[0].shape[0]]
-        newModel = modelSol.y[0]
-        t = modelSol.t
-    else:
-        newModel = modelSol.y[0][0:actualSol.y[0].shape[0]]
-        newTrue = actualSol.y[0]
-        t = actualSol.t
-      
-    diff = np.abs(newTrue - newModel)
-    plt.plot(t, diff)
